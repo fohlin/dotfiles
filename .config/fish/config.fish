6aa6330afced9bdd620df05da8a333cd303d2041
Fredrik Ohlin <fo@fredrikohlin.com>
2020-07-27 16:12:43 +0200

    Set preferred PATH order

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,13 @@
 fundle plugin 'edc/bass'
 fundle init
 source ~/.iterm2_shell_integration.fish
+# PATH
+set -p fish_user_paths "/sbin"
+set -p fish_user_paths "/usr/sbin"
+set -p fish_user_paths "/bin"
+set -p fish_user_paths "/usr/bin"
 set -p fish_user_paths "/usr/local/sbin"
+set -p fish_user_paths "/usr/local/bin"
 # Configure gpg
 set -g GPG_TTY (tty)
 # could automatically use default node version using
