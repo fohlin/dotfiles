b822b7849ea18d5ba99d0906193875958e59a79f
Fredrik Ohlin <fo@fredrikohlin.com>
2020-07-27 16:13:23 +0200

    Opt out of dotnet cli telemetry

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,6 +8,8 @@ set -p fish_user_paths "/bin"
 set -p fish_user_paths "/usr/bin"
 set -p fish_user_paths "/usr/local/sbin"
 set -p fish_user_paths "/usr/local/bin"
+# Other env vars
+set -gx DOTNET_CLI_TELEMETRY_OPTOUT 'true'
 # Configure gpg
 set -g GPG_TTY (tty)
 # could automatically use default node version using
